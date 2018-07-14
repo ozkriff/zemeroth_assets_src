56773c8b3da2b7406c14f459215cdf73841b081c
Andrey Lesnikov <ozkriff@gmail.com>
2018-07-14 15:50:52 +0300

    atlas.svg + export.py

---
+++ export.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""Convert one `.svg` file to many `png`s.
+
+TODO: document it.
+"""
+
+import subprocess
+import os
+
+
+# TODO: extract to some json (?) config
+ids = [
+    'tile',
+    'tile_rocks',
+    'poison_cloud',
+    'imp',
+    'imp_toxic',
+    'imp_summoner',
+    'imp_bomber',
+    'grass',
+    'shadow',
+    'fire',
+    'boulder',
+    'bomb',
+    'bomb_fire',
+    'bomb_poison',
+    'bomb_demonic',
+    'blood',
+    'spike_trap',
+    'dot',
+    'selection',
+    'white_hex',
+    'hammerman',
+    'spearman',
+    'alchemist',
+    'swordsman',
+
+    # TODO:
+    # 'test_button_bg',
+
+    # new sprites
+    'eye',  # TODO: rename to 'beholder'
+    'snake',
+    'grave_stone',
+    'axeman',
+]
+out_dir_name = 'png'
+input_file_name = 'atlas.svg'  # TODO: make configurable
+
+
+# ./export.py && cp png/* ~/zemeroth/main/assets
+
+
+def _main() -> None:
+    os.makedirs(out_dir_name, exist_ok=True)
+    for id in ids:
+        cmd = [
+            'inkscape',
+            input_file_name,
+            f'--export-id={id}',
+            f'--export-png={out_dir_name}/{id}.png',
+            # f'--export-dpi={96 * 4}',
+        ]
+        print(cmd)
+        # TODO: check resulting image size. I'm expecting 256x256, right?
+        subprocess.run(cmd, check=True)
+
+
+_main()
